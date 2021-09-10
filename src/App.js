bc715837efd7c2dd766ec31938c904871d11cf52
Prashant Singh <v-prsingh@microsoft.com>
2021-09-10 07:52:12 +0530

    Home Page with Navigation menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter, NavLink, Route } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
 function App() {
+  const home = () => {
+    return <div>Home</div>;
+  };
+  const about = () => {
+    return <div>About</div>;
+  };
+  const contact = () => {
+    return <div>Contact</div>;
+  };
+  const services = () => {
+    return <div>Services</div>;
+  };
+  const work = () => {
+    return <div>Work</div>;
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HashRouter>
+        <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+              <Navbar.Brand href="#">Portfolio</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ml-auto">
+                  <NavLink to="/" className="active">
+                    Home
+                  </NavLink>
+                  <NavLink to="/about">About</NavLink>
+                  <NavLink to="/services">Services</NavLink>
+                  <NavLink to="/work">Work</NavLink>
+                  <NavLink to="/contact">Contact</NavLink>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </div>
+        <div className="content">
+          <Route exact path="/" component={home} />
+          <Route path="/about" component={about} />
+          <Route path="/services" component={services} />
+          <Route path="/work" component={work} />
+          <Route path="/contact" component={contact} />
+        </div>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './navbar.modules.css';
+
+const Navigationbar = () => {
+return (
+	<>
+
+	</>
+);
+};
+
+export default Navigationbar;
