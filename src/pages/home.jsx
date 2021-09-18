c34a05b0b9f8a2912df8c392157cfb9a090bfadd
Prashant Singh <v-prsingh@microsoft.com>
2021-09-18 11:36:48 +0530

    Name Plate

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,11 @@
 import Circle from "../assets/Circle.png"
+import "./home.css"
  const home = () => {
     return (
       <div className="home">
         <div className="name">
           <h1>&#60;Prashant Singh/&#62;</h1>
-          <span>Web Designer</span>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro veniam repellat neque unde assumenda quam beatae voluptas doloribus labore, ipsam eos temporibus tenetur iste dolor corporis consequatur iusto dolores? Voluptate!</p>
         </div>
         <div className="mandal">
           <img src={Circle} className="outerpiece" />
