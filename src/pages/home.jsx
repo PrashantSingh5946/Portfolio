5b766efb471a75e366489d007651b56b8d8d7133
Prashant Singh <prashantsingh@Prashants-MacBook-Pro.local>
2021-11-17 04:48:09 -0800

    Removing flower from home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,9 +7,6 @@ import "./home.css"
           <h1>&#60;Prashant Singh<span>/</span>&#62;</h1>
           <p>Hi, I am Prashant Singh a full stack developer and i love making websites.I have a wide range of skills along with a background in API and UI automtion.</p>
         </div>
-        <div className="mandal">
-          <img src={Circle} className="outerpiece" />
-        </div>
       </div>
     );
 };
